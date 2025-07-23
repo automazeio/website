ad57c4433ec6ac4b033e1b2614cb0333140204a7
Ran Aroussi <ran@aroussi.com>
2025-07-23 11:06:08 +0100

    Remove deprecated components including AlertBox, CancelClosure, EmptyStateNoData, FormErrors, FormRow, Pagination, TransactionRow, and ValidationAlert for improved codebase clarity and maintainability.

--- app/views/components/AlertBox.php
+++
@@ -1,9 +0,0 @@
-<?php
-tiny::components()->register('AlertBox', function (...$props) {
-    $class = $props['class'] ?? '';
-    $level = $props['level'] ?? '';
-    $return = "<div class=\"alert {$level} {$class}\">";
-    $return .= addslashes($props['content'] ?? '');
-    $return .= "</div>";
-    return $return;
-});
