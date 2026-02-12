ec1b58f04a8fde805955c597adffd53adcbdc32f
Ran Aroussi <ran@aroussi.com>
2026-02-12 15:02:12 +0000

    Refactor Sentry script inclusion logic in open.php for improved clarity

--- app/views/layouts/default/open.php
+++ app/views/layouts/default/open.php
@@ -5,7 +5,7 @@ echo '<!-- v. '. $_SERVER['APP_VERSION'] ." -->\n";
 <html lang="en" class="relative min-h-full overscroll-none"  style="scroll-behavior: smooth"
     x-data="{ expandedService: null }">
 <head>
-<?php if (@$_SERVER['ENV'] != 'local' && @$_SERVER['SENTRY_FRONTEND']): ?>
+<?php if (@$_SERVER['ENV'] != 'local'): ?>
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-7NKW2LYNSQ"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
@@ -25,7 +25,9 @@ echo '<!-- v. '. $_SERVER['APP_VERSION'] ." -->\n";
     <!-- rb2b -->
     <script>!function () {var reb2b = window.reb2b = window.reb2b || [];if (reb2b.invoked) return;reb2b.invoked = true;reb2b.methods = ["identify", "collect"];reb2b.factory = function (method) {return function () {var args = Array.prototype.slice.call(arguments);args.unshift(method);reb2b.push(args);return reb2b;};};for (var i = 0; i < reb2b.methods.length; i++) {var key = reb2b.methods[i];reb2b[key] = reb2b.factory(key);}reb2b.load = function (key) {var script = document.createElement("script");script.type = "text/javascript";script.async = true;script.src = "https://s3-us-west-2.amazonaws.com/b2bjsstore/b/" + key + "/reb2b.js.gz";var first = document.getElementsByTagName("script")[0];first.parentNode.insertBefore(script, first);};reb2b.SNIPPET_VERSION = "1.0.1";reb2b.load("R6G5YHZ47765");}();</script>
 
-    <!-- <script src="https://js.sentry-cdn.com/<?php echo $_SERVER['SENTRY_FRONTEND']; ?>.min.js" crossorigin="anonymous"></script> -->
+    <?php if (@$_SERVER['SENTRY_FRONTEND']): ?>
+        <script src="https://js.sentry-cdn.com/<?php echo $_SERVER['SENTRY_FRONTEND']; ?>.min.js" crossorigin="anonymous"></script>
+    <?php endif; ?>
 <?php endif; ?>
     <script>
         window.trackDiscoveryCallClick = function (location) {
