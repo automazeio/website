151d4ce04759f1ac1a3cbcbe70ceb987b67eb7d3
Ran Aroussi <ran@aroussi.com>
2026-05-27 12:35:12 +0100

    Remove unused logo and spacing elements from discovery call page

--- app/views/discovery-call.php
+++ app/views/discovery-call.php
@@ -11,13 +11,10 @@
     } */
 </style>
 <div class="flex flex-col items-center justify-center h-full">
-    <div class="bg-black rounded-full border size-20 shrink-0 hidden md:block" style="padding:0.75rem; margin-bottom:1rem"><img loading="lazy" src="<?php tiny::staticURL('img/logo-light.svg'); ?>" alt="Automaze logo" /></div>
     <!-- iClosed inline widget begin -->
     <div class="iclosed-widget" data-url="https://app.iclosed.io/e/automazeio/intro" title="🤙 Intro call" style="width: 100%; height:620px; max-height:100%"></div>
     <script type="text/javascript" src="https://app.iclosed.io/assets/widget.js" async></script>
     <!-- iClosed inline widget end -->
-
-    <p class="hidden md:block">&nbsp;<br>&nbsp;<br>&nbsp;<br>&nbsp;<br>&nbsp;<br>&nbsp;</p>
 </div>
 
 <?php tiny::layout()->default('/'); ?>
